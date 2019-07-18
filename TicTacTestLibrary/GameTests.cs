9970f0fe6dcb35aacc5b00a108bc026375a6b0a7
StephanieK <stephanie.kitchen@serko.com>
2019-07-18 13:59:13 +1200

    Game tests for a new instance of game

--- TicTacTestLibrary/GameTests.cs
+++ TicTacTestLibrary/GameTests.cs
@@ -9,7 +9,7 @@ namespace TicTacTestLibrary
     public class GameTests
     {
         [Fact]
-        public void GivenNewGame_ReturnsEmptyGameboard()
+        public void GivenNewGame_ReturnsEmptyGameboard ()
         {
             Game game = new Game();
             var expectedGameboard = new BoardPiece[,] {
@@ -17,17 +17,25 @@ namespace TicTacTestLibrary
                 { BoardPiece.Empty , BoardPiece.Empty , BoardPiece.Empty } ,
                 { BoardPiece.Empty , BoardPiece.Empty , BoardPiece.Empty } };
 
-            Assert.Equal(expectedGameboard, game.board.board);
+            Assert.Equal( expectedGameboard , game.board.board );
         }
 
         [Fact]
-        public void GivenNameGame_WinnerAsCurrentPlayerExpectedX()
+        public void GivenNewGame_ReturnsCurrentPlayerX()
         {
             Game game = new Game();
-            var expectedWinner = BoardPiece.X;
+            var expectedStartPlayer = BoardPiece.X;
 
-            game.StartGame();
-            Assert.Equal( expectedWinner , game.currentPlayer.Character );
+            Assert.Equal( expectedStartPlayer , game.currentPlayer.Character );
+        }
+
+        [Fact]
+        public void GivenNewGame_ReturnsnextMoveAsZero()
+        {
+            Game game = new Game();
+
+            Assert.Equal( 0 , game.nextMove.Row );
+            Assert.Equal( 0 , game.nextMove.Column );
         }
     }
 }
