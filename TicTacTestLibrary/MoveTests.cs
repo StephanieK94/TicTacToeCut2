d93c1c3e47cbae531b0fa28abac363145ceef83d
StephanieK <stephanie.kitchen@serko.com>
2019-07-31 17:32:04 +1200

    Renaming MoveTests to ensure catches exception

--- TicTacTestLibrary/MoveTests.cs
+++ TicTacTestLibrary/MoveTests.cs
@@ -8,40 +8,48 @@ namespace TicTacTestLibrary
 {
     public class MoveTests
     {
-         [Fact]
-        public void GivenInput_ReturnMove()
-        {
-            int row = 1;
-            int column = 1;
+        private readonly Move nextMove;
 
-            Move nextMove = new Move();
+        public MoveTests()
+        {
+            nextMove = new Move();
+        }
+
+        [Theory]
+        [InlineData( 1 , 1 , true )]
+        [InlineData( 2 , 2 , true )]
+        [InlineData( 3 , 3 , true )]
+        [InlineData( 0 , 0 , false )]
+        [InlineData( 4 , 4 , false )]
+        [InlineData( -1 , -1 , false )]
+        public void GivenInput_WhenValidationForRangeCalled_ReturnsExpectedResult(int row, int column, bool expectedResult)
+        {
             nextMove.Row = row;
             nextMove.Column = column;
 
-            Assert.Equal(0, nextMove.Row);
-            Assert.Equal(0, nextMove.Column);
+            var actual = nextMove.ValidatePlayerMoves( nextMove );
+
+            Assert.Equal(expectedResult, actual);
         }
 
         [Theory]
         [InlineData( "q1,2" , true )]
         [InlineData( "1,2", false)]
         [InlineData( "1,Q2", true)]
+        [InlineData( "1,P", false)]
         public void GivenInput_WhenCheckForForfeit_ReturnsExpectedOutput(string input, bool output)
         {
-            Move nextMove = new Move();
-
             bool playerForfeits = nextMove.CheckForForfeit( input );
 
             Assert.Equal( output, playerForfeits );
         }
 
         [Fact]
-        public void GivenInvalidInput_WhenTriesToConvertMove_ReturnsFalse()
+        public void GivenInvalidInput_WhenTriesToConvertMove_CatchesExceptionAndReturnsFalse()
         {
             var input = ",1";
 
-            var move = new Move();
-            var isValidInput = move.ConvertPlayerInputToMove( input );
+            var isValidInput = nextMove.ConvertPlayerInputToMove( input );
 
             Assert.False( isValidInput );
         }
