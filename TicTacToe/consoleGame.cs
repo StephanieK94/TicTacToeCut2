c5de7d4585a27b6c14b41c38076b198be8fd7c6a
StephanieK <stephanie.kitchen@serko.com>
2019-10-16 16:48:56 +1300

    Removal of the logic within the ConsoleGame to be a model

--- TicTacToe/consoleGame.cs
+++ TicTacToe/consoleGame.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks.Dataflow;
@@ -7,79 +8,15 @@ using TicTacToe.Lib;
 
 namespace TicTacToe.ConsoleApplication
 {
-    public class consoleGame
+    public class ConsoleGame
     {
         public int TurnCount { get; set; }
-        public ConsoleBoard Board { get; set; }
+        public string[] Board { get; set; }
         public string CurrentPlayer { get; set; }
         public ConsoleMove CurrentMove { get; set; }
-
         public ConsoleWinCalculator WinCalculator { get; set; }
         public ConsoleWriter ConsoleWriter { get; set; }
-        //public MessageProcessor Message { get; set; }
         public Messages Message { get; set; }
         public ConsoleReader ConsoleReader { get; set; }
-
-        // TODO: Remove this into the service?
-        public consoleGame ()
-        {
-            TurnCount = 0;
-            Board = Factory.CreateConsoleBoard();
-            CurrentPlayer = "";
-            CurrentMove = Factory.CreateNewConsoleMove();
-            WinCalculator = Factory.CreateConsoleWinCalculator(); 
-            ConsoleWriter = Factory.CreateConsoleWriter();
-            ConsoleReader = Factory.CreateConsoleReader();
-            Message = Factory.CreateMessageList();
-        }
-
-        public bool PlayMove(int currentMove)
-        {
-            if (Board.Layout[currentMove] != "") return false;
-            Board.Layout[currentMove] = CurrentPlayer;
-            return true;
-        }
-        public void StartGame ()
-        {
-            ConsoleWriter.PrintOutput(Message.Welcome());
-            ConsoleWriter.PrintBoard(Board.Layout);
-        }
-
-        public bool ValidateInput ( string playerInput )
-        {
-            if ( CurrentMove.CheckForForfeit( playerInput ) == true )
-            {
-                ChangePlayer();
-                WinCalculator.IsWinner = true;
-                return true;
-            }
-
-            if ( CurrentMove.CanConvertPlayerInputToMove( playerInput ) == false )
-            {
-                ConsoleWriter.PrintOutput( Message.InvalidInput());
-                return false;
-            }
-
-            if ( CurrentMove.ValidateMoveWithinRange() == false )
-            {
-                ConsoleWriter.PrintOutput( Message.OutOfBounds() );
-                return false;
-            }
-            CurrentMove.ConvertToPosition();
-            return true;
-        }
-
-        // TODO: Remove this and refactor so a part of the tic tac toe service?
-        public bool PromptForNewGame ()
-        {
-            ConsoleWriter.PrintOutput( Message.PromptForNewGame() );
-            var playAgain = ConsoleReader.GetInput();
-            return playAgain.ToLower().Contains( "Y".ToLower() );
-        }
-
-        public void ChangePlayer()
-        {
-            CurrentPlayer = ( CurrentPlayer == "X") ? CurrentPlayer = "O" : CurrentPlayer = "X";
-        }
     }
 }
